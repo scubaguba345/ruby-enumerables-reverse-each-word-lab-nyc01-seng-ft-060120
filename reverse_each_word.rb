fdb31e98ec16bb022deaacc7fe00a58f1a2cc6ce
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-04-10 18:41:05 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(string)
+
+    split_string = string.split("")
+    reversed = []
+    string.size.times { reversed << split_string.pop }
+    reversed.join
+end
+
+puts reverse_each_word
