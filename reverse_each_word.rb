cb113a6616c6258e94a77095442042f312ddaeee
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-04-10 19:02:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(string)
 
     split_string = string.split(" ")
     reversed_word = []
-    reversed_word = split_string.each {|word| word.reverse }
+    reversed_word = split_string.collect {|word| word.reverse }
     reversed_word.join(" ")
 end
